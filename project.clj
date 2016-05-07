51e3f018292cd65b6c5fa35ec2857274b80121ae
Johan Mena <johan.menac@gmail.com>
2016-05-07 13:14:35 -0400

    Support JSON

--- project.clj
+++ project.clj
@@ -2,11 +2,16 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.0"]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-json "0.4.0"]]
   :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler clerk.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+  :global-vars {*warn-on-reflection* true}
+  :ring {:handler clerk.handler/app
+         :nrepl {:start? true :port 3001}}
+  :main clerk.handler
+  :aot [clerk.handler]
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring/ring-mock "0.3.0"]]}})
--- src/clerk/handler.clj
+++ src/clerk/handler.clj
@@ -1,11 +1,20 @@
 (ns clerk.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.middleware.json :refer [wrap-json-body wrap-json-response]])
+  (:gen-class))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+      (wrap-defaults api-defaults)
+      (wrap-json-body {:keywords? true})
+      (wrap-json-response)))
+
+(defn -main [& args]
+  (run-jetty app {:port 3000}))
